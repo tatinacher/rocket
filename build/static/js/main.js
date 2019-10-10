56822d5d1a645a9eb3dcac850eb90fa2c2fe9867
Tatina Cherednichenko <tatinacher@gmail.com>
2019-10-10 14:41:17 +0300

    Create project with gulp, sass, pug. Add first page.

---
+++ build/static/js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    window.addEventListener('scroll', function(e) {
+        //document.getElementById('showScroll').innerHTML = pageYOffset + 'px';
+        console.log(e);
+    });
+});
