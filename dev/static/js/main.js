01fa1b1ec10d770eeed4017ec565d6fee632f099
Tatina Cherednichenko <tatinacher@gmail.com>
2019-10-11 17:55:08 +0300

    Close question block if click on another one

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
     $(".toggle-btn").click(function() {
+        $(".question__text").each(function() {
+            $(this).addClass("hidden");
+        })
         $(this).closest(".question").find(".question__text").toggleClass("hidden");  
         $(this).find(".icon").toggleClass("close");  
     });
     $(".nav-item").click(function() {
         $(".nav-item").each(function() {
             $(this).removeClass("active");
-          })
+        });
         $(this).toggleClass("active");
     });
     $(".mobile-menu-burger").click(function() {
